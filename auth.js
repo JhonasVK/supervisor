f0780b9c39721f5750201004f6bd35b8ac1dbad9
Jhonas VK <jhonasvk@users.noreply.github.com>
2026-09-22 14:00:33 -0300

    auth: nuevo usuario eganzalez

--- auth.js
+++ auth.js
@@ -26,7 +26,8 @@
     amanrriquez: '5e3020f9f2c77ad69039767c6921ae00760010ffc26dc13400a0c2e592cf0e05',
     jbaez:       '4c444b9fe4bb2ece193c6ddafadca08b0c1e226cc087fc171f68c2e33544baf1',
     cquiroz:     '790cc118f6e5c7a78b7b4016feb1212978f42fb048f05634373b9e7cb8c24ccb',
-    fflores:     'bc541b07a9bb72311d725b4e847b811c1286f1aa3aeba5c1ad50773a53d48e0c'
+    fflores:     'bc541b07a9bb72311d725b4e847b811c1286f1aa3aeba5c1ad50773a53d48e0c',
+    eganzalez:   'd3da32e7a49252f3471d5d29c05d92f0160acfcaa86818b7957d25497592c2cd'
   };
 
   function ocultar()  { try { document.documentElement.style.visibility = 'hidden'; } catch (e) {} }
